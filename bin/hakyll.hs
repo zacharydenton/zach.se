acf8055928ef1e56c6ac339ceda26880693f7c38
Zach Denton <zacharydenton@gmail.com>
2012-12-26 23:09:08 -0500

    Add current time to all pages.

--- bin/hakyll.hs
+++ bin/hakyll.hs
@@ -4,97 +4,112 @@ module Main where
 import Prelude hiding (id)
 import Control.Category (id)
 import Control.Arrow ((>>>), (***), arr)
+import Control.Monad (liftM)
 import Data.Monoid (mempty, mconcat)
 import Data.List (isPrefixOf, isSuffixOf)
+import Data.Time
 import System.FilePath
+import System.Locale
 import Text.Pandoc
 import Hakyll
 
 main :: IO ()
-main = hakyllWith config $ do
-    match "templates/*" $ compile templateCompiler
+main = do
+    currentTime <- liftM (formatTime defaultTimeLocale "%Y-%m-%dT%H:%M:%SZ") getCurrentTime
+    hakyllWith config $ do
+        match "templates/*" $ compile templateCompiler
 
-    match "static/css/**" $ do
-        route $ setRoot `composeRoutes` setExtension "css"
-        compile sass
+        match "static/css/**" $ do
+            route $ setRoot `composeRoutes` setExtension "css"
+            compile sass
 
-    match "static/js/**.coffee" $ do
-        route $ setRoot `composeRoutes` setExtension "js"
-        compile coffeescript
+        match "static/js/**.coffee" $ do
+            route $ setRoot `composeRoutes` setExtension "js"
+            compile coffeescript
 
-    match (predicate (\i -> matches "static/**" i && not (matches "static/css/**" i) && not (matches "static/js/**.coffee" i))) $ do
-        route $ setRoot
-        compile copyFileCompiler
+        match (predicate (\i -> matches "static/**" i && not (matches "static/css/**" i) && not (matches "static/js/**.coffee" i))) $ do
+            route $ setRoot
+            compile copyFileCompiler
 
-    -- compile posts twice to avoid dependency cycle
-    
-    posts <- group "posts" $ match "posts/*" $ do
-        route $ postRoute `composeRoutes` cleanURL
-        compile $ pageCompiler
-            >>> arr (renderDateField "fulldate" "%Y-%m-%d" "Unknown")
-            >>> arr (renderDateField "date" "%b %e %Y" "Unknown")
-            >>> arr (renderDateField "year" "%Y" "Unknown")
-            >>> arr stripIndexLink
+        -- compile posts twice to avoid dependency cycle
+        
+        posts <- group "posts" $ match "posts/*" $ do
+            route $ postRoute `composeRoutes` cleanURL
+            compile $ postsCompiler
+                >>> arr stripIndexLink
 
-    _ <- match "posts/*" $ do
-        route $ postRoute `composeRoutes` cleanURL
-        compile $ nicePageCompiler
-            >>> renderModificationTime "updated" "%Y-%m-%d"
-            >>> arr (renderDateField "fulldate" "%Y-%m-%d" "Unknown")
-            >>> arr (renderDateField "date" "%b %e %Y" "Unknown")
-            >>> arr (copyBodyToField "description")
-            >>> setFieldPageList (take 8 . recentFirst) "templates/postitem.html" "recentposts" posts
-            >>> applyTemplateCompiler "templates/post.html"
+        match "posts/*" $ do
+            route $ postRoute `composeRoutes` cleanURL
+            compile $ postsCompiler
+                >>> setFieldPageList (take 8 . recentFirst) "templates/postitem.html" "recentposts" posts
+                >>> applyTemplateCompiler "templates/post.html"
+                >>> applyTemplateCompiler "templates/default.html"
+
+        match "pages/**" $ do
+            route $ setRoot `composeRoutes` cleanURL
+            compile $ metaCompiler
+                >>> setFieldPageList (take 8 . recentFirst) "templates/postitem.html" "recentposts" posts
+                >>> applyTemplateCompiler "templates/page.html"
+                >>> applyTemplateCompiler "templates/default.html"
+
+        match "archives.html" $ route cleanURL
+        create "archives.html" $ constA mempty
+            >>> arr (setField "title" "Archives")
+            >>> arr (setField "bodyclass" "archives")
+            >>> arr (setField "excerpt" "All posts on the site.")
+            >>> setFieldPageList ((filter (\p -> (getField "year" p) == "2010")) . recentFirst) "templates/postitem.html" "posts2010" posts
+            >>> setFieldPageList ((filter (\p -> (getField "year" p) == "2011")) . recentFirst) "templates/postitem.html" "posts2011" posts
+            >>> setFieldPageList ((filter (\p -> (getField "year" p) == "2012")) . recentFirst) "templates/postitem.html" "posts2012" posts
+            >>> applyTemplateCompiler "templates/archives.html"
             >>> applyTemplateCompiler "templates/default.html"
 
-    match "pages/**" $ do
-        route $ setRoot `composeRoutes` cleanURL
-        compile $ nicePageCompiler
-            >>> renderModificationTime "updated" "%Y-%m-%d"
-            >>> setFieldPageList (take 8 . recentFirst) "templates/postitem.html" "recentposts" posts
-            >>> applyTemplateCompiler "templates/page.html"
+        match "index.html" $ route idRoute
+        create "index.html" $ constA mempty
+            >>> arr (setField "title" "Home")
+            >>> arr (setField "bodyclass" "home")
+            >>> arr (setField "excerpt" "On life, the universe, and everything.")
+            >>> setFieldPageList (take 1 . recentFirst) "templates/raw.html" "latest" posts
+            >>> setFieldPageList (take 8 . tail . recentFirst) "templates/postitem.html" "recentposts" posts
+            >>> applyTemplateCompiler "templates/index.html"
             >>> applyTemplateCompiler "templates/default.html"
 
-    match "archives.html" $ route cleanURL
-    create "archives.html" $ constA mempty
-        >>> arr (setField "title" "Archives")
-        >>> arr (setField "bodyclass" "archives")
-        >>> setFieldPageList (take 8 . recentFirst) "templates/postitem.html" "recentposts" posts
-        >>> setFieldPageList ((filter (\p -> (getField "year" p) == "2010")) . recentFirst) "templates/postitem.html" "posts2010" posts
-        >>> setFieldPageList ((filter (\p -> (getField "year" p) == "2011")) . recentFirst) "templates/postitem.html" "posts2011" posts
-        >>> setFieldPageList ((filter (\p -> (getField "year" p) == "2012")) . recentFirst) "templates/postitem.html" "posts2012" posts
-        >>> applyTemplateCompiler "templates/archives.html"
-        >>> applyTemplateCompiler "templates/default.html"
+        match "sitemap.xml" $ route idRoute
+        create "sitemap.xml" $ constA mempty
+            >>> arr (setField "updated" currentTime)
+            >>> setFieldPageList id "templates/postsitemap.xml" "posts" posts
+            >>> setFieldPageList (map (arr stripIndexLink)) "templates/postsitemap.xml" "pages" "pages/**"
+            >>> applyTemplateCompiler "templates/sitemap.xml"
 
-    match "index.html" $ route idRoute
-    create "index.html" $ constA mempty
-        >>> arr (setField "title" "Zach Denton")
-        >>> setFieldPageList (take 1 . recentFirst) "templates/raw.html" "latest" posts
-        >>> setFieldPageList (take 8 . tail . recentFirst) "templates/postitem.html" "recentposts" posts
-        >>> applyTemplateCompiler "templates/index.html"
-        >>> applyTemplateCompiler "templates/default.html"
+        match "atom.xml" $ route idRoute
+        create "atom.xml" $ requireAll_ posts >>> renderAtom feedConfiguration
 
-    match "sitemap.xml" $ route idRoute
-    create "sitemap.xml" $ constA mempty
-        >>> requireAllA "posts/*" postListSitemap
-        >>> requireAllA "pages/**" pageListSitemap
-        >>> applyTemplateCompiler "templates/sitemap.xml"
+        match "rss.xml" $ route idRoute
+        create "rss.xml" $ requireAll_ posts >>> renderRss feedConfiguration
 
-    match "rss.xml" $ route idRoute
-    create "rss.xml" $
-        requireAll_ "posts/*"
-            >>> renderRss feedConfiguration
-
-    match "templates/*" $ compile templateCompiler
+        match "templates/*" $ compile templateCompiler
 
 -- compilers
 
-nicePageCompiler :: Compiler Resource (Page String)
-nicePageCompiler = pageCompilerWith
+defaultCompiler :: Compiler Resource (Page String)
+defaultCompiler = pageCompilerWith
     defaultHakyllParserState
     defaultHakyllWriterOptions {
         writerHTMLMathMethod = MathJax ""
     }
+
+metaCompiler :: Compiler Resource (Page String)
+metaCompiler = defaultCompiler
+    >>> arr (trySetField "excerpt" "")
+    >>> arr (trySetField "bodyclass" "")
+    >>> arr (setField "root" "http://zacharydenton.com")
+    >>> renderModificationTime "updated" "%Y-%m-%dT%H:%M:%SZ"
+    >>> arr (copyBodyToField "description")
+
+postsCompiler :: Compiler Resource (Page String)
+postsCompiler = metaCompiler
+    >>> arr (renderDateField "published" "%Y-%m-%d" "Unknown")
+    >>> arr (renderDateField "date" "%b %e %Y" "Unknown")
+    >>> arr (renderDateField "year" "%Y" "Unknown")
 
 sass :: Compiler Resource String
 sass = getResourceString
@@ -126,23 +141,6 @@ fileToDirectory = (flip combine) "index.html" . dropExtension . toFilePath
 
 stripIndexLink :: Page a -> Page a
 stripIndexLink = changeField "url" dropFileName
-
-postList :: Compiler (Page String, [Page String]) (Page String)
-postList = buildList "posts" "templates/postitem.html"
-
-postListSitemap :: Compiler (Page String, [Page String]) (Page String)
-postListSitemap = buildList "posts" "templates/postsitemap.xml"
-
-pageListSitemap :: Compiler (Page String, [Page String]) (Page String)
-pageListSitemap = buildList "pages" "templates/postsitemap.xml"
-
-buildList :: String -> Identifier Template -> Compiler (Page String, [Page String]) (Page String)
-buildList field template = setFieldA field $
-    arr (reverse . chronological)
-        >>> arr (map stripIndexLink)
-        >>> require template (\p t -> map (applyTemplate t) p)
-        >>> arr mconcat
-        >>> arr pageBody
 
 config :: HakyllConfiguration
 config = defaultHakyllConfiguration {
